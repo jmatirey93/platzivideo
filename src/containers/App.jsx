cf8bd8dc29df4fdb85522022ad772ffb832bd30f
jmatirey93 <jmatirey93@gmail.com>
2021-01-22 14:22:11 +0000

    Adding API, use of React Hooks and PropTypes

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,49 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from  '../components/Footer'
+import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
+const API = 'http://localhost:3000/initalState';
 
-        <Categories title="Mi lista"> 
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+const App = () => {
+    const initialState = useInitialState(API);
 
-        <Categories title="Tendencias">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+    return (
+        <div className="App">
+            <Header />
+            <Search />
+            {initialState.mylist.length > 0 &&
+                            <Categories title="Mi lista">
+                            <Carousel>
+                                <CarouselItem />
+                            </Carousel>
+                        </Categories>
+            }
 
-        <Categories title="Recomendaciones">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+            <Categories title="Tendencias">
+                <Carousel>
+                    {
+                        initialState.trends.map(item => 
+                            <CarouselItem key={item.id} {...item} />
+                        )
+                    }
+                </Carousel>
+            </Categories>
 
-        <Footer />
-    </div>
-);
+            <Categories title="Recomendaciones">
+                <Carousel>
+                    {
+                        initialState.originals.map(item =>
+                            <CarouselItem key= {item.id} {...item} />
+                            )
+                    }
+                    
+                </Carousel>
+            </Categories>
+
+            <Footer />
+        </div>
+    );
+}
 
 export default App;
