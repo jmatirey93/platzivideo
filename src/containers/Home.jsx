479255ce3c6b6ed196c4a3a3eacf389a264af47e
jmatirey93 <jmatirey93@gmail.com>
2021-01-22 15:31:53 +0000

    Creating routes folder and some modifications

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -10,7 +10,7 @@ import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initalState';
 
-const App = () => {
+const Home = () => {
     const initialState = useInitialState(API);
 
     return (
@@ -51,4 +51,4 @@ const App = () => {
     );
 }
 
-export default App;
+export default Home;
